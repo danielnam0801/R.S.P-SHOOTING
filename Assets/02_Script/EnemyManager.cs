1e20032700f6c324ae136664a0bb1898e88bbb10
danielnam0801 <danielnam0801@gmail.com>
2022-06-01 23:34:26 +0900

    EnemyManage 스크립트 추가

--- Assets/02_Script/EnemyManager.cs
+++ Assets/02_Script/EnemyManager.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
-{
+{ 
     [SerializeField]
-    GameObject _rock;
+    GameObject[] enemies;
     [SerializeField]
-    GameObject _scissor;
-    [SerializeField]
-    GameObject _paper;
-    [SerializeField]
-    private float spawnTime;
+    public Transform[] spawnPoints;
+    public float maxSpawnTIme;
+    public float curSpawnTIme;
 
 
     // Start is called before the first frame update
@@ -23,6 +21,20 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        curSpawnTIme += Time.deltaTime;
         
+        if(curSpawnTIme > maxSpawnTIme)
+        {
+            SpawnEnemy();
+            maxSpawnTIme = Random.Range(0.5f, 3f);
+            curSpawnTIme = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0 , 3);
+        int ranPoint = Random.Range(0 , 5);
+        Instantiate(enemies[ranEnemy] , spawnPoints[ranPoint].position  , spawnPoints[ranPoint].rotation);
     }
 }
